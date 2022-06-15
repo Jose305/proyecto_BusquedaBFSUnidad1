38c506b64fbb44c1b1894eda2a92f551b108819e
Jose305 <jaruiz17@espe.edu.ec>
2022-06-14 22:09:01 -0500

    Version 3

--- AlgoritmoBFS.py
+++ AlgoritmoBFS.py
@@ -9,3 +9,29 @@
     Programación de la busqueda BFS
     Agente: Agrobot E-Series cosechador de fresas
 """
+# importación del modulo collections
+from collections import deque
+
+class grafo:
+    '''
+    Clase que representa un objeto grafico segun los datos otorgados
+
+    '''
+    # Constructor
+    def __init__(self, bordes, numero_nodos):
+        '''
+        Contructor que inicializa el problema de busqueda
+
+        @param bordes:  relaciona un nodo con su nodo vecino
+        @param n: numero de nodos del grafo.
+        
+        '''
+ 
+        # crea una lista de aristas para representar una lista de adyacencia
+        self.lista_adja = [[] for _ in range(numero_nodos)]
+ 
+        # agrega bordes al gráfico no dirigido
+        for (origen, destino) in bordes:
+            self.lista_adja[origen].append(destino)
+            self.lista_adja[destino].append(origen)
+            
