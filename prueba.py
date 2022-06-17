b548dd631c1a52542ab9105fc0ac0458b0c145c6
Jose305 <jaruiz17@espe.edu.ec>
2022-06-17 09:22:53 -0500

    Prueba unitaria

---
+++ prueba.py
@@ -0,0 +1,11 @@
+import unittest
+from collections import deque
+import AlgoritmoBFS
+
+class PruebaUnitaria(unittest.TestCase):
+    
+      def test_grafo(self):
+          pass
+
+if __name__ == "__main__":
+    unittest.main()
